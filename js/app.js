d6497f14eda898f611b1b964c326b8d27aae4916
Subrata Sarker <subrata_sarker@yahoo.com>
2019-08-07 00:08:19 +0900

    Use components for control, number and operators button

--- js/app.js
+++ js/app.js
@@ -30,87 +30,99 @@ const Display = props => (
   </div>
 );
 
+const ControlPad = () => (
+  <div className='row'>
+    <div id='clear' className='col-6 clear'>
+      <button className='btn ctrl ac'>AC</button>
+    </div>
+    <div className='col-3'>
+      <button className='btn ctrl ce'>CE</button>
+    </div>
+    <div id='equals' className='col-3'>
+      <button className='btn result'>=</button>
+    </div>
+  </div>
+);
+
+const NumberPad = () => (
+  <div className='row'>
+    <div id='seven' className='col-4'>
+      <button className='btn dgt seven'>7</button>
+    </div>
+    <div id='eight' className='col-4'>
+      <button className='btn dgt eight'>8</button>
+    </div>
+    <div id='nine' className='col-4'>
+      <button className='btn dgt nine'>9</button>
+    </div>
+    <div id='four' className='col-4'>
+      <button className='btn dgt four'>4</button>
+    </div>
+    <div id='five' className='col-4'>
+      <button className='btn dgt five'>5</button>
+    </div>
+    <div id='six' className='col-4'>
+      <button className='btn dgt six'>6</button>
+    </div>
+    <div id='one' className='col-4'>
+      <button className='btn dgt one'>1</button>
+    </div>
+    <div id='two' className='col-4'>
+      <button className='btn dgt two'>2</button>
+    </div>
+    <div id='three' className='col-4'>
+      <button className='btn dgt three'>3</button>
+    </div>
+    <div id='negate' className='col-4'>
+      <button className='btn soptr' value='+/-'>
+        +/-
+      </button>
+    </div>
+    <div id='zero' className='col-4'>
+      <button className='btn dgt zero'>0</button>
+    </div>
+    <div id='decimal' className='col-4'>
+      <button className='btn dot'>.</button>
+    </div>
+  </div>
+);
+
+const OperatorsPad = () => (
+  <div className='row'>
+    <div id='divide' className='col-12'>
+      <button className='btn optr obelus' value='/'>
+        &divide;
+      </button>
+    </div>
+    <div id='multiply' className='col-12'>
+      <button className='btn optr times' value='*'>
+        &times;
+      </button>
+    </div>
+    <div id='subtract' className='col-12'>
+      <button className='btn optr minus' value='-'>
+        -
+      </button>
+    </div>
+    <div id='add' className='col-12'>
+      <button className='btn optr plus' value='+'>
+        +
+      </button>
+    </div>
+  </div>
+);
+
 const KeyPad = () => (
   <div className='row'>
     <div className='col-12'>
       <div className='calculator-buttons'>
-        <div className='row'>
-          <div id='clear' className='col-6 clear'>
-            <button className='btn ctrl ac'>AC</button>
-          </div>
-          <div className='col-3'>
-            <button className='btn ctrl ce'>CE</button>
-          </div>
-          <div id='equals' className='col-3'>
-            <button className='btn result'>=</button>
-          </div>
-        </div>
+        <ControlPad />
         <div className='row'>
           <div className='col-9'>
-            <div className='row'>
-              <div id='seven' className='col-4'>
-                <button className='btn dgt seven'>7</button>
-              </div>
-              <div id='eight' className='col-4'>
-                <button className='btn dgt eight'>8</button>
-              </div>
-              <div id='nine' className='col-4'>
-                <button className='btn dgt nine'>9</button>
-              </div>
-              <div id='four' className='col-4'>
-                <button className='btn dgt four'>4</button>
-              </div>
-              <div id='five' className='col-4'>
-                <button className='btn dgt five'>5</button>
-              </div>
-              <div id='six' className='col-4'>
-                <button className='btn dgt six'>6</button>
-              </div>
-              <div id='one' className='col-4'>
-                <button className='btn dgt one'>1</button>
-              </div>
-              <div id='two' className='col-4'>
-                <button className='btn dgt two'>2</button>
-              </div>
-              <div id='three' className='col-4'>
-                <button className='btn dgt three'>3</button>
-              </div>
-              <div id='negate' className='col-4'>
-                <button className='btn soptr' value='+/-'>
-                  +/-
-                </button>
-              </div>
-              <div id='zero' className='col-4'>
-                <button className='btn dgt zero'>0</button>
-              </div>
-              <div id='decimal' className='col-4'>
-                <button className='btn dot'>.</button>
-              </div>
-            </div>
+            <NumberPad />
           </div>
           <div className='col-3'>
-            <div className='row'>
-              <div id='divide' className='col-12'>
-                <button className='btn optr obelus' value='/'>
-                  &divide;
-                </button>
-              </div>
-              <div id='multiply' className='col-12'>
-                <button className='btn optr times' value='*'>
-                  &times;
-                </button>
-              </div>
-              <div id='subtract' className='col-12'>
-                <button className='btn optr minus' value='-'>
-                  -
-                </button>
-              </div>
-              <div id='add' className='col-12'>
-                <button className='btn optr plus' value='+'>
-                  +
-                </button>
-              </div>
-            </div>
+            <OperatorsPad />
           </div>
         </div>
       </div>
